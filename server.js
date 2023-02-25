ca5c569f4d7286f60ef9f2a8b5985a75e06d2542
ruslanpetrovup <ruslanpetrov163@gmail.com>
2023-02-25 19:13:11 +0200

    added api

--- server.js
+++ server.js
@@ -2,53 +2,43 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const app = express();
-// const OpenAI = require("openai");
 require("dotenv").config();
-// const openai = new OpenAI(process.env.TOKEN_API);
+const app = express();
 const TelegramBot = require("node-telegram-bot-api");
-const token = "853161826:AAGXKhWdrHVexfWj_Gmlgp40wOtoh8SaGMc";
-console.log(process.env.TOKEN_API);
-
-const bot = new TelegramBot(token, {
-  polling: { autoStart: true, params: { allowed_updates: true } },
-});
+const bot = new TelegramBot(process.env.TOKEN_BOT, { polling: true });
 
 app.use(cors());
 app.use(bodyParser.json());
 
 bot.on("message", async (msg) => {
   if (msg.text === "/start") {
-    bot.sendMessage(msg.chat.id, "Напиши мне что-то");
+    bot.sendMessage(msg.chat.id, "Напиши мне свой город");
     return;
   }
+  const chatId = msg.chat.id;
+  const CITY_NAME = msg.text;
+
+  console.log(process.env.TOKEN_WEATHER);
+
+  const locationResponse = await axios.get(
+    `https://api.weatherbit.io/v2.0/current?city=${CITY_NAME}&key=${process.env.TOKEN_WEATHER}`
+  );
+  console.log(locationResponse);
+  const weatherData = response.data.data[0];
+
+  console.log(
+    `Current temperature in ${CITY_NAME} is ${weatherData.temp}°C (${weatherData.app_temp}°C feels like).`
+  );
+
+  bot.sendMessage(
+    chatId,
+    `Current temperature in ${CITY_NAME} is ${weatherData.temp}°C (${weatherData.app_temp}°C feels like).`
+  );
 });
 
 app.use("/", (req, res) => {
   res.send("Тут пусто. Иди в бот");
 });
-
-// app.use("/", async (req, res) => {
-//   const { text } = req.body;
-//   const gptResponse = await openai.complete({
-//     engine: "davinci",
-//     prompt: text,
-//     maxTokens: 600,
-//     temperature: 0.5,
-//     topP: 0.9,
-//     presencePenalty: 0,
-//     frequencyPenalty: 0,
-//     bestOf: 3,
-//     n: 1,
-//     model: "text-davinci-002",
-//     stream: false,
-//     stop: text.split(" ")[0],
-//   });
-
-//   console.log(text.split(" ")[0]);
-
-//   res.send(gptResponse.data.choices[0].text);
-// });
 
 app.listen(8000, () => {
   console.log("server start");
