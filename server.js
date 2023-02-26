ef1109ebf0c4c4acbc9704f8fb81fe6a1d7059ae
ruslanpetrovup <ruslanpetrov163@gmail.com>
2023-02-26 02:54:35 +0200

    fix router

--- server.js
+++ server.js
@@ -10,39 +10,49 @@ const bot = new TelegramBot(process.env.TOKEN_BOT, { polling: true });
 app.use(cors());
 app.use(bodyParser.json());
 
+bot.setMyCommands([{ command: '/weather', description: 'Узнать погоду в своем городе' }]);
 bot.on('message', async (msg) => {
-  if (msg.text === '/start') {
-    bot.sendMessage(msg.chat.id, 'Напиши мне свой город');
+  if (msg.text === '/weather') {
+    const chatId = msg.chat.id;
+    bot.onText(/\/weather/, async (msg) => {
+      return await bot.sendMessage(chatId, 'Напиши названия нужного города').then(() => {
+        return bot.onText(/^(.+)$/, async function listenInput(msg, match) {
+          try {
+            const locationResponse = await axios.get(
+              `https://api.weatherbit.io/v2.0/current?city=${match.input}&key=${process.env.TOKEN_WEATHER}`
+            );
+            const weatherData = locationResponse.data.data[0];
+
+            await bot.sendPhoto(
+              chatId,
+              `https://www.weatherbit.io/static/img/icons/${weatherData.weather.icon}.png`
+            );
+
+            bot.sendMessage(
+              chatId,
+              `Текущая температура в городе ${weatherData.city_name} составляет ${weatherData.temp}°C (ощущается как ${weatherData.app_temp}°C).`
+            );
+            bot.removeTextListener('text', listenInput);
+            bot.removeTextListener(/^(.+)$/);
+            bot.removeTextListener(/\/weather/);
+            return;
+          } catch (err) {
+            bot.sendMessage(
+              chatId,
+              'Произошла ошибка, попробуйте еще раз! Может вы ввели не правильно название города'
+            );
+            return;
+          }
+        });
+      });
+    });
+    bot.downloadFile();
     return;
   }
-  const chatId = msg.chat.id;
-  const CITY_NAME = msg.text;
 
-  console.log(process.env.TOKEN_WEATHER);
-
-  try {
-    const locationResponse = await axios.get(
-      `https://api.weatherbit.io/v2.0/current?city=${CITY_NAME}&key=${process.env.TOKEN_WEATHER}`
-    );
-    const weatherData = locationResponse.data.data[0];
-
-    await bot.sendPhoto(
-      chatId,
-      `https://www.weatherbit.io/static/img/icons/${weatherData.weather.icon}.png`
-    );
-    console.log(
-      `Текущая температура в городе ${weatherData.city_name} составляет ${weatherData.temp}°C (ощущается как ${weatherData.app_temp}°C).`
-    );
-
-    bot.sendMessage(
-      chatId,
-      `Текущая температура в городе ${weatherData.city_name} составляет ${weatherData.temp}°C (ощущается как ${weatherData.app_temp}°C).`
-    );
-  } catch (err) {
-    bot.sendMessage(
-      chatId,
-      'Произошла ошибка, попробуйте еще раз! Может вы ввели не правильно название города'
-    );
+  if (msg.text === '/start') {
+    bot.sendMessage(msg.chat.id, 'Открой меню и выбери нужное действие');
+    return;
   }
 });
 
